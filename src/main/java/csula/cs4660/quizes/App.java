ec6de1e95435da8fb251d7f9623c33556d611658
Rowan Edge <rowan.edge@gmail.com>
2016-10-16 22:35:12 -0700

    BFS implemented

--- src/main/java/csula/cs4660/quizes/App.java
+++ src/main/java/csula/cs4660/quizes/App.java
@@ -1,5 +1,7 @@
 package csula.cs4660.quizes;
 
+import csula.cs4660.quizes.models.DTO;
+import csula.cs4660.quizes.models.Event;
 import csula.cs4660.quizes.models.State;
 
 import java.util.*;
@@ -13,12 +15,13 @@ public class App {
         State initialState = Client.getState("10a5461773e8fd60940a56d2e9ef7bf4").get();
         System.out.println(initialState);
         // to get an edge between state to its neighbor, you can call stateTransition
-        System.out.println(Client.stateTransition(initialState.getId(), initialState.getNeighbors()[0].getId()));
+        // System.out.println(Client.stateTransition(initialState.getId(), initialState.getNeighbors()[0].getId()));
 
         Queue<State> frontier = new LinkedList<>();
         Set<State> exploredSet = new HashSet<>();
         Map<State, State> parents = new HashMap<>();
         frontier.add(initialState);
+
 
         while (!frontier.isEmpty()) {
             State current = frontier.poll();
@@ -28,8 +31,31 @@ public class App {
             for (State neighbor: Client.getState(current.getId()).get().getNeighbors()) {
                 // state transition
                 if (neighbor.getId().equals("e577aa79473673f6158cc73e0e5dc122")) {
+                    System.out.println(neighbor);
                     // construct actions from endTile
                     System.out.println("found solution with depth of " + findDepth(parents, current, initialState));
+                    int cost = 0;
+                    ArrayList<State> backwardsPath = new ArrayList<State>();
+                    backwardsPath.add(neighbor);
+                    backwardsPath.add(current);
+                    while (!parents.get(backwardsPath.get(backwardsPath.size() - 1)).equals(initialState)) {
+                        backwardsPath.add(parents.get(backwardsPath.get(backwardsPath.size() - 1)));
+                    }
+                    backwardsPath.add(initialState);
+
+                    for (int i = backwardsPath.size() - 1; i >= 0; i--) {
+                        if (i > 0) {
+                            DTO d = Client.stateTransition(backwardsPath.get(i).getId(), backwardsPath.get(i - 1).getId()).get();
+                            System.out.println(backwardsPath.get(i).getLocation().getName() + " : "
+                            + backwardsPath.get(i - 1).getLocation().getName() + " : "
+                            + d.getEvent().getEffect() + ", path: "
+                            + backwardsPath.get(i).getId() + " -> "
+                            + backwardsPath.get(i - 1).getId());
+                            cost += d.getEvent().getEffect();
+                        }
+                    }
+                    System.out.println("Total cost: " + cost);
+                    return;
                 }
                 if (!exploredSet.contains(neighbor)) {
                     parents.put(neighbor, current);
@@ -37,8 +63,7 @@ public class App {
                 }
             }
         }
-
-        System.out.println("Not found solution");
+        System.out.println("Fully explored");
     }
 
     public static int findDepth(Map<State, State> parents, State current, State start) {
